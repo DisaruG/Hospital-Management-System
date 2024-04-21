0f456ae2a2fcf16f3e9a5fa9965448d209aa2b2e
Disaru Gunawardhana <disaru1314@gmail.com>
2024-04-21 14:58:31 +0530

    Update Billing.java

--- hospitalmanagementsystem/src/main/java/org/example/Billing.java
+++ hospitalmanagementsystem/src/main/java/org/example/Billing.java
@@ -1,4 +1,47 @@
 package org.example;
 
 public class Billing {
+    private int id;
+    private Patient patient;
+    private double amount;
+    private boolean paid;
+
+    public Billing(int id, Patient patient, double amount, boolean paid) {
+        this.id = id;
+        this.patient = patient;
+        this.amount = amount;
+        this.paid = paid;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
 }
